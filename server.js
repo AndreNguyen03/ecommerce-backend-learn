350d4e88d217d6a7b09bd99a31ba552c8dec6072
AndreNguyen03 <123562712+AndreNguyen03@users.noreply.github.com>
2025-03-30 16:10:16 +0700

    test pr v02

--- server.js
+++ server.js
@@ -6,6 +6,6 @@ const server = app.listen(PORT, () => {
   console.log(`WSV eCommerce start with ${PORT}`);
 });
 
-process.on(`SIGINT`, () => {
-  server.close(() => console.log(`Exit Server Express`));
-});
+// process.on(`SIGINT`, () => {
+//   server.close(() => console.log(`Exit Server Express`));
+// });
