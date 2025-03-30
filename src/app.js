8fd85cb1b6b2921c0252a06f075cdb194099d8d3
AndreNguyen03 <123562712+AndreNguyen03@users.noreply.github.com>
2025-03-30 16:19:32 +0700

    test pr filechanges v02

--- src/app.js
+++ src/app.js
@@ -35,13 +35,13 @@ app.use((req, res, next) => {
   next(error);
 });
 
-// app.use((error, req, res, next) => {
-//   const statusCode = error.status || 500;
-//   return res.status(statusCode).json({
-//     status: `error`,
-//     code: statusCode,
-//     message: error.message || `Internal Server Error`,
-//   });
-// });
+app.use((error, req, res, next) => {
+  const statusCode = error.status || 500;
+  return res.status(statusCode).json({
+    status: `error`,
+    code: statusCode,
+    message: error.message || `Internal Server Error`,
+  });
+});
 
 export default app;
